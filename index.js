0e273f66cddd42f17aae4e6dbd3434aa2084460d
Gitonga Dennis <dennydennis777@gmail.com>
2019-10-26 12:06:39 +0300

    added routes imported to index use method

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routes = require('./routes/api');
 //set up express app
 const app = express();
+//using use method on our app
+app.use('/api', routes);
 //listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listen for requests');
